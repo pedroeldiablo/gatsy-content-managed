a532d654c2b9d8c0a59c4eebc6bda9bc22f67395
Peter Williams <pedroeldiablo@yahoo.com>
2020-05-18 15:14:04 +0100

    Add additional eslint and using Gasby Link for internal links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 export const AboutPage = () => {
     return (
         <>
             <h1>About</h1>
             <h2>An about summary goes here</h2>
+            <p><Link to='/contact'>Find me.</Link></p>
+            <p><a href="https://github.com/pedroeldiablo">Find me on Github</a></p>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export const IndexPage = () => {
     return (
         <>
             <h1>Hello.</h1>
             <h2>Welcome to a Gatsby Site</h2>
+            <p><Link to='/contact'>Find out more.</Link></p>
+            <p><a href="https://github.com/pedroeldiablo">Find me on Github</a></p>
         </>
     )
 }
