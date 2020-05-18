a63b4a7688128b0c008481dfb0ca3f91cf7e9bec
Peter Williams <pedroeldiablo@yahoo.com>
2020-05-18 15:15:17 +0100

    Making external link open in new tab

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export const AboutPage = () => {
             <h1>About</h1>
             <h2>An about summary goes here</h2>
             <p><Link to='/contact'>Find me.</Link></p>
-            <p><a href="https://github.com/pedroeldiablo">Find me on Github</a></p>
+            <p><a href="https://github.com/pedroeldiablo" target="_blank">Find me on Github</a></p>
         </>
     )
 }
