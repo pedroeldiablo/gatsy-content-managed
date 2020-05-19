50c83e07aec80b14fc460861f5f654c7d7bfe26f
Peter Williams <pedroeldiablo@yahoo.com>
2020-05-19 12:15:00 +0100

    Applying header stayles using css-modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,48 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import headerStyles from './header.module.scss'
 
 export const Header = () => {
   return (
     <header>
       <h1>Manage Me</h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navbar}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              to="/"
+              className={headerStyles.link}
+              activeClassName={headerStyles.linkActive}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              className={headerStyles.link}
+              activeClassName={headerStyles.linkActive}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              to="/blog"
+              className={headerStyles.link}
+              activeClassName={headerStyles.linkActive}
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              to="/contact"
+              className={headerStyles.link}
+              activeClassName={headerStyles.linkActive}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
