cb21ba498b6fbc839b3271426008af1f3474ff03
Peter Williams <pedroeldiablo@yahoo.com>
2020-05-19 14:41:24 +0100

    setting up sample blog posts and using gatsby transformer remark to display summary

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,29 @@ module.exports = {
     title: 'Gatsby Content Managed',
     author: 'pedroeldiablo',
   },
-  plugins: ['gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+  ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import { Layout } from '../components/layout'
 
 export const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        totalCount
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              description
+              date
+            }
+            excerpt
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <h1>Blog</h1>
       <p>Posts will show up here later once set-up</p>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.id}>
+            <h2>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </h2>
+          </Link>
+          <p>{node.excerpt}</p>
+          <Link to={node.id}>Go to blog</Link>
+          <p>Time to read: {node.timeToRead}</p>
+        </div>
+      ))}
     </Layout>
   )
 }
