c11d7eab128948cf896d0563826a8d96e022fdec
Peter Williams <pedroeldiablo@yahoo.com>
2020-06-08 23:57:29 +0100

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import { Head } from '../components/head'
 export const NotFound = () => (
   <Layout>
     <Head title="404" />
-    <h1>Oh deer page not found</h1>
+    <h1>404 - nothing here my dear</h1>
     <p>
       <Link to="/">Take me home.</Link>
     </p>
