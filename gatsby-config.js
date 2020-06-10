a06e73ce24ea12cb0fd40d1a4fdc5e11a1ebe5ae
Peter Williams <pedroeldiablo@yahoo.com>
2020-06-10 23:53:43 +0100

    update favicon and include manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,29 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `CYMK`,
+        short_name: `CYMK`,
+        description: `Do cool things and make your life better.`,
+        lang: `en`,
+        display: `standalone`,
+        icon: `src/images/cmyk.png`,
+        start_url: `/`,
+        background_color: `#FF00FF`,
+        theme_color: `#fff`,
+        localize: [
+          {
+            start_url: `/de/`,
+            lang: `de`,
+            name: `Die coole Anwendung`,
+            short_name: `Coole Anwendung`,
+            description: `Mach coole Dinge und mach dein Leben besser`,
+          },
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
