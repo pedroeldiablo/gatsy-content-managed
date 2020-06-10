71e7511aec5435065dc341c0f313ac666e5718e7
Peter Williams <pedroeldiablo@yahoo.com>
2020-06-10 20:13:07 +0100

    Syarting styling with styled-components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 import Layout from '../components/layout'
 import { Head } from '../components/head'
+import { Title } from './index'
 
 export const AboutPage = () => {
   return (
     <Layout>
       <Head title="About"></Head>
-      <h1>About</h1>
+      <Title>About</Title>
       <h2>An about summary goes here</h2>
       <p>
         <Link to="/contact">Find me.</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 import { Layout } from '../components/layout'
 import { Head } from '../components/head'
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
 
 export const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
-      <h1>Hello.</h1>
+      <Title>Hello.</Title>
       <h2>Welcome to a Gatsby Site</h2>
       <p>
         <Link to="/contact">Find out more.</Link>
