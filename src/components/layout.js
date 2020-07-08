cf5b17626f2353442d668264405f3b868f953cf2
Peter Williams <pedroeldiablo@yahoo.com>
2020-07-08 17:30:43 +0100

    introducing styled syste and setting up themed context

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,17 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
+import theme from '../theme/theme'
 import { Header } from './header'
 import { Footer } from './footer'
 import '../styles/index.scss'
 
+console.log({ theme })
+
 const LayoutContainer = styled.div`
+  color: ${theme.colors.body};
+  background-color: ${theme.colors.white};
   @media screen and (min-width: 780px) {
     width: 66vw;
     display: grid;
@@ -14,17 +19,18 @@ const LayoutContainer = styled.div`
     grid-template-columns: auto 1fr 1fr 1fr 0.5fr;
     grid-template-rows: auto 1fr 1fr auto auto;
     grid-gap: 1rem;
-    /* transform: rotate(-45deg); */
   }
 `
 
 export const Layout = props => {
   return (
-    <LayoutContainer>
-      <Header></Header>
-      {props.children}
-      <Footer></Footer>
-    </LayoutContainer>
+    <ThemeProvider theme={theme}>
+      <LayoutContainer>
+        <Header></Header>
+        {props.children}
+        <Footer></Footer>
+      </LayoutContainer>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    body: '#000e1a',
+    white: '#fff',
+    blue: '#007ce0',
+    navy: '#004175',
+    brand: '#ff0011',
+  },
+}
+
+export default theme
