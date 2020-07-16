d7bfc89e7ee71d1b7e129c60141cb1eb2d344ace
Peter Williams <pedroeldiablo@yahoo.com>
2020-07-16 15:10:06 +0100

    initial styling for footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,6 +22,16 @@ const wobble = keyframes`
 `
 
 const FooterComponent = styled.footer`
+  grid-column: 1 /6;
+  grid-row-end: -1;
+  border-bottom: 25px solid red;
+  align-self: start;
+  justify-content: start;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* grid-template-columns: auto minmax(20px, auto) 1fr 1fr 1fr 0.5fr; */
+  grid-template-rows: auto 1fr 1fr auto auto;
+  grid-gap: 1rem;
   width: 100%;
   display: flex;
   color: black;
@@ -33,16 +43,38 @@ const FooterComponent = styled.footer`
 `
 
 const Paragraph = styled.p`
-  &:hover {
     background: pink;
     color: blue;
     /* animation: ${wobble} 2s linear infinite; */
     a {
-      color: red;
+      text-decoration: none;
+      /* color: red; */
       display: inline-block;
-      animation: ${wobble} 2s linear infinite;
+      &:hover {
+      /* animation: ${wobble} 2s linear infinite; */
+      box-decoration-break: clone;
+      -webkit-box-decoration-break: clone;
+      box-shadow: 15px -15px 0px -10px rgba(255, 000, 000, 1),
+    -5px -5px 0px 0 rgba(00, 000, 00, 1),
+    -5px 5px 0px 0 rgba(00, 000, 00, 1),
+    5px 5px 1px 0 rgba(255, 000, 000, 1);
     }
   }
+`
+
+const ATagComponent = styled.a`
+  padding: 1rem;
+  /* color: red; */
+  display: inline-block;
+  box-decoration-break: clone;
+  -webkit-box-decoration-break: clone;
+  box-shadow: 20px -10px 1px -5px rgba(255, 000, 000, 0.2),
+    -5px -5px 1px 0 rgba(00, 000, 00, 0.5),
+    5px 5px 1px 0 rgba(255, 255, 000, 0.5),
+    5px 5px 1px 0 rgba(000, 255, 000, 0.5);
+
+  /* box-shadow: 8px 8px 10px 0 #ff1492, -5px -5px 5px 0 #00f, 5px 5px 15px 0 #ff0; */
+  /* border-radius: 16px; */
 `
 
 export const Footer = () => {
@@ -63,13 +95,13 @@ export const Footer = () => {
       </Paragraph>
       <Paragraph>
         Some text outside the link
-        <a
+        <ATagComponent
           href=" https://cmykool.netlify.app/"
           target="_blank"
           rel="noreferrer"
         >
           See me online
-        </a>
+        </ATagComponent>
       </Paragraph>
       <Paragraph>
         <a
