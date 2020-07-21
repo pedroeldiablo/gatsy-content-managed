c9facc54c53a88597923dced4792bee9b993635f
Peter Williams <pedroeldiablo@yahoo.com>
2020-07-21 22:40:08 +0100

    Removing console logs and passing on props to children

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,36 +1,30 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 const getColor = props => {
-  console.log('Evaluated', props.isActive)
-  console.log('Evaluated', props)
   if (props.isActive) {
     return 'red'
   }
   return 'white'
 }
 
-console.log(getColor)
-
 const ButtonComponent = styled.button`
-${console.log(getColor)}
   background: ${getColor};
   padding: 1rem;
 `
 
 const DivStyle = styled.div`
-  /* background: ${getColor}; */
+  background: ${getColor};
 `
 
 export const Button = (props: ButtonProps) => {
-  // console.log('What are the children?', children)
   const { text } = props
-  console.log('What are the props?', props)
-  console.log(props.isActive)
   return (
     <ButtonComponent {...props}>
       <div>What is going on here? {text}</div>
-      <DivStyle>{`What is going on here? ${props.isActive}`}</DivStyle>
+      <DivStyle {...props}>{`What is going on here? ${
+        props.isActive ? 'This what active looks like' : 'Nothing set'
+      }`}</DivStyle>
     </ButtonComponent>
   )
 }
